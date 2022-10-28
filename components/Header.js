0cfc8969b6d7e6afe60ced613bce1ab1f216444b
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2022-10-29 00:56:25 +0200

    Small fix on logo

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,7 @@ function Header() {
     <header className="flex justify-between p-5 max-w-7xl mx-auto ">
         <div className="flex items-center space-x-5">
             <Link href="/">
-                <h1 className="text-3xl cursor-pointer font-sans font-bold text-blue">Obi-<span className="text-darkdarkblue">One.io</span></h1>
+                <h1 className="text-3xl cursor-pointer font-sans font-bold text-blue">Obi<span className="text-darkdarkblue">One.io</span></h1>
             </Link>     
         </div>    
         <div>
