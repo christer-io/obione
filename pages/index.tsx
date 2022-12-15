4b64e2f87ed1407dd8ab3438041bbd2cbfb602d4
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2022-12-15 18:44:50 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,7 +49,7 @@ export default function Home ({ posts, technical, external, question  }: Props) 
         {posts.map((post) => (
           <Link key={post._id} href={`/post/${post.slug.current}`}>
             <div className="border text-wihte border-lightgray rounded-lg group cursor-pointer overflow-hidden">
-              <img className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out" src={urlFor(post.mainImage).url()!} alt={post.title} />
+              <img className="h-40 lg:h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out" src={urlFor(post.mainImage).url()!} alt={post.title} />
               <div className="flex justify-between p-5 bg-white">
                 <div>
                   <p className="text-lg font-bold text-darkblue">{post.title}</p>
