fb631ef3a42551f3a0253bdde1bfe1697f7febda
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2022-12-30 03:19:34 +0100

    large update

--- sanity.js
+++ sanity.js
@@ -4,7 +4,8 @@ import imageUrlBuilder from "@sanity/image-url"
 export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ,
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    useCdn: process.env.NODE_ENV === "production",
+    useCdn: true,
+    apiVersion: '2022-12-22',
 };
 
 export const sanityClient = createClient(config);
