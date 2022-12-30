a7ef968b176b764c9eb350fb4560b48f984b44b6
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2022-12-30 09:35:15 +0100

    Trying fix without CDNs

--- sanity.js
+++ sanity.js
@@ -4,7 +4,7 @@ import imageUrlBuilder from "@sanity/image-url"
 export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ,
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    useCdn: true,
+    useCdn: false,
     apiVersion: '2022-12-22',
 };
 
