d853f6e9f2b77dc06c4676ce5bbbee586bd0e7c1
petterwilhelm <petter@keyteq.no>
2023-01-25 12:33:01 +0100

    handle form submit, enter

--- components/Search.js
+++ components/Search.js
@@ -20,16 +20,20 @@ function Search() {
       });
   };
 
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    searching();
+  }
+
   
   return (
     <div className="flex justify-center p-3 mx-auto ">
-       
         <div className=" p-1 space-x-2">
-          <input className="rounded-xl p-2 w-50 sm:w-60 border" placeholder="Enter search terms" onChange={(e) => setSearch(e.target.value)}></input>
-          <button onClick={searching} className="text-white pt-2 pb-2 pr-3 pl-3 bg-maxdarkblue rounded-xl border border-white">Search</button>  
+          <form onSubmit={handleFormSubmit}>
+            <input className="rounded-xl p-2 w-50 sm:w-60 border" placeholder="Enter search terms" onChange={(e) => setSearch(e.target.value)}></input>
+            <button onClick={searching} className="text-white pt-2 pb-2 pr-3 pl-3 bg-maxdarkblue rounded-xl border border-white">Search</button>  
+          </form>
         </div> 
-            
-         
     </div>
   )
 }
