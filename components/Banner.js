c8caac5b0875ad5f7cf705b498a021242a362ba6
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2023-02-27 14:24:20 +0100

    Small fixes to frontend

--- components/Banner.js
+++ components/Banner.js
@@ -5,8 +5,8 @@ function Banner() {
     <div className='pb-8'>
     <section class="bg-gray-800 pt-6">
     <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
-        <a href="https://chat.obione.io" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
-            <span class="text-xs bg-[#4D8FE4] rounded-full text-white px-4 py-1.5 mr-3">New</span> <span class="text-sm font-medium">Check out our new ChatGPT replica!</span> 
+        <a href="#" class="hidden sm:inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
+            <span class="text-xs bg-red-800 rounded-full text-white px-4 py-1.5 mr-3">New</span> <span class="text-sm font-medium">This page is under construction!</span> 
             <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
         </a>
         <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-7xl dark:text-white pl-10 pr-10"> <span className='text-[#4D8FE4]'>Be the change</span> that inspires change!</h1>
