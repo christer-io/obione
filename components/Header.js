73966fc7afe8bdd06cb1b9f306797c0d3bade304
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2023-07-29 14:07:25 +0200

    Fix in header

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,7 @@ function Header() {
               </a>
           
               <div className="flex items-center lg:order-2">
-                  <a href="https://chat.obione.io" className="text-white  font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 hover:bg-gray-700 focus:outline-none focus:ring-gray-800">ObiOne ChatGPT</a>
+                  <a href="https://obione.io" className="text-white  font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 hover:bg-gray-700 focus:outline-none focus:ring-gray-800"></a>
               </div>
               <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                   <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
